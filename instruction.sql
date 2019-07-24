d28295a8dc832767fd17e7b123a8c671a31f3083
EY-X <e.yosefx128@gmail.com>
2019-07-24 15:51:47 -0400

    First commit

---
+++ instruction.sql
@@ -0,0 +1,50 @@
+Exerciase 1:
+
+Question 1:
+
+SELECT name FROM robots WHERE source='Star Wars';
+
+Question 2:
+
+SELECT name FROM robots WHERE personality='anxious';
+
+Question 3:
+
+SELECT name FROM recipes WHERE nut_free='t';
+
+Question 4:
+
+SELECT COUNT(name) FROM recipes WHERE gluten_free=TRUE AND vegetarian=FALSE;
+
+Question 5:
+
+SELECT MAX(number_of_legs) FROM animals;
+
+Question 6:
+
+SELECT MIN(mins_to_play) FROM board_games;
+
+Question 7:
+
+SELECT MAX(minutes_required) from recipes;
+
+Question 8:
+
+SELECT name FROM robots WHERE name LIKE 'M%';
+
+Question 9:
+
+SELECT name FROM board_games WHERE max_players <= 8;
+
+Question 10:
+
+SELECT name FROM animals WHERE swimming=TRUE AND egg_laying=TRUE;
+
+Question 11:
+
+SELECT name FROM animals WHERE swimming=TRUE AND egg_laying=TRUE AND flying=FALSE;
+
+Question 12:
+
+SELECT MAX(max_players) from board_games;
+
